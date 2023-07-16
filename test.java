b9088e8b248609113b770fda076fa43bea755dae
sumedhbajra <128482228+sumedhbajra@users.noreply.github.com>
2023-07-16 11:37:54 +0545

    Create test.java
    This is my Java Code.

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test{
+  public static void main([] args){
+    System.out.println("This world is unfair");
+    String newStr = mySpace();
+    System.out.println(newStr);
+  }
+  public static String mySpace(){
+    String a = "This world is mine.";
+    System.out.println("Hello WORLD Hahaha");
+    return a;
+  }
+}
